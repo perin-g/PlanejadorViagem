a720c5ac3c9dd03be6322e0a5ca15ab885f8056f
perin-g <gabriel.2015012222@alunocesul.com>
2026-07-29 22:26:01 -0300

    Criada main e feita configuração inicial

--- src/main/java/br/cesul/MainApp.java
+++ src/main/java/br/cesul/MainApp.java
@@ -1,13 +1,26 @@
 package br.cesul;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
- * Hello world!
- *
+ * Classe principal, só carrega o FXML e exibe na tela
+ * Não deve conter nenhuma lógica de domínio
  */
-public class MainApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Usaremos o FXMLLoader para carregar o layout da tela a partir do arquivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/cesul/trip_view.fxml"));
+        Parent root = loader.load();
+
+        stage.setTitle("TripPlanner - MVC");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
+
+    public static void main( String[] args ) { launch(args); }
 }
